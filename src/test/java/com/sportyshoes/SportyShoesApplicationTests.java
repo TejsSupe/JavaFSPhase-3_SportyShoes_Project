e82ec96aa1b00fd46022796d9a7ef530b79ac251
TejsSupe <supe.tejas@gmail.com>
2022-04-23 15:07:01 +0530

    project commit

---
+++ src/test/java/com/sportyshoes/SportyShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportyshoes;
+
+import org.tejassupe.tejs02.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
